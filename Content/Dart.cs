46748330dcd214153ff41591dd2dee6abe1a2548
AE517 <guilherme.hjaraujo@proton.me>
2025-01-31 12:13:22 -0300

    Doc Dart code logic

--- Content/Dart.cs
+++ Content/Dart.cs
@@ -4,8 +4,18 @@ using Terraria.ModLoader;
 
 namespace PlentyODarts.Content
 {
+    /// <summary>
+    /// A Subclass of the Vanilla Throwing Class inheriting ModItem.
+    /// This class works as Base for any Dart in the mod.
+    /// As a subclass it'll inherit every already in-place effect and modifiers present, but also
+    /// providing access to unique modifiers and Modus.
+    /// </summary>
     public abstract class DartWeapon : ModItem
     {
+        /// <summary>
+        /// This gives access to Modus behavior changing effects on the Shoot hook, it is enabled by default.
+        /// If false, the Shoot hook will follow any previous (or default) logic inside it.
+        /// </summary>
         protected virtual bool IsModusApplicable => true;
 
         //Ensures Reforge prefixes to actually work also including the Dart class directed ones;
@@ -31,6 +41,12 @@ namespace PlentyODarts.Content
             return true;
         }
 
+        /// <summary>
+        /// Applies the current equiped Modus to the Dart in use (HeldItem) changing it's present Shoot logic.
+        /// </summary>
+        /// <param name="player">Actual player (Projectile.owner) using the held Dart.</param>
+        /// <param name="position">Dart Spawn Projectile initial position (defined upon shooting).</param>
+        /// <param name="velocity">Dart Spawn Projectile initial speed (defined upon shooting at the Item.shootspeed in SetDefault + any modifier present).</param>
         private static bool ApplyModus(Player player, Vector2 position, Vector2 velocity)
         {
             return player.GetModPlayer<PoDPlayer>().CurrentModus.Modus(player, position, velocity)
--- obj/Debug/net8.0/PlentyODarts.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlentyODarts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlentyODarts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcb562e64f2a8c41d2badad20c44a5cf9c17c3f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb6ad01a9078cba3c3791b5c025cf4058ee43c72")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlentyODarts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlentyODarts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
